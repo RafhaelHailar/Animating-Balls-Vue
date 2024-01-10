427a1e027e4b28a7ec1414b9f6ce3ccabb71abc0
Rafhael <rafhael4hailar@gmail.com>
2024-01-10 15:02:11 +0800

    Adding some styles and new animations

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            animation: "rotate-x"
+            animation: "rotate-x",
+            overflow: "hidden"
         }
     },
     methods: {
         changeAnimation(animation) {
             this.animation = animation; 
+            
+            if ((animation.split("-"))[0] == "slide") this.overflow = "hidden";
+            else this.overflow = "auto";
         }
     }
 });
